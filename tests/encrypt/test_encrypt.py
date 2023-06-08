19d0b2363fa5ee65c337c2ff37269f8288ab9a7c
AIMÊ <aimemartins@hotmail.com>
2023-06-08 15:31:46 -0300

    Requisito 2

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,14 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError):
+        encrypt_message(1, 2)
+        encrypt_message(1, "mesagem")
+        encrypt_message("mensagem", "mensagem")
+    assert encrypt_message("mensagem", -1) == "megasnem"
+    assert encrypt_message("mensagem", 2) == "megasn_em"
+    assert encrypt_message("mensagem", 4) == "mega_snem"
+    assert encrypt_message("mensagem", 9) == "megasnem"
+    assert encrypt_message("mensagem", 3) == "nem_megas"
