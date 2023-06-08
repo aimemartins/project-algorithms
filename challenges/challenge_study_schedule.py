ac0c0da4200426eb9592c246f238594a85cd9503
AIMÊ <aimemartins@hotmail.com>
2023-06-08 12:25:45 -0300

    requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if target_time is None:
+        return None
+    indices_encontrados = 0
+    for input, output in permanence_period:
+        if not isinstance(input, int) or not isinstance(output, int):
+            return None
+        if input <= target_time <= output:
+            indices_encontrados += 1
+    return indices_encontrados
+    # raise NotImplementedError
+
+
+# print(study_schedule([(10, 11), (8, 12), (12, 22)], 12))
