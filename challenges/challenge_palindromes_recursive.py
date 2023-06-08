421a908a8d76989e0472c8496104a8bb5f7a51e6
AIMÊ <aimemartins@hotmail.com>
2023-06-08 16:03:12 -0300

    Requisito 3

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,12 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not word:
+        return False
+    # condição de parada
+    if low_index >= high_index:
+        return True
+    if word[low_index] != word[high_index]:
+        return False
+    # chamada recursiva
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+    # raise NotImplementedError
+    # ajuda de Joseane Oliveira
